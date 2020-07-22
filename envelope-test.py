c1a92186bf3c556599ea8e9408e3986f7a163534
Raymond Viviano <rayviviano@gmail.com>
2020-07-22 00:50:13 -0400

    Create envelope-test.py
    Script to test if random spline generator works as expected for amplitude envelopes

---
+++ envelope-test.py
@@ -0,0 +1,24 @@
+# Amplitude envelope test.
+#
+# Basically plots out randomly generated envelopes to test
+# if the spline-generating code works as expected.
+#
+# Raymond Viviano
+# July 22nd, 2020
+
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+from .mudpie-sample-generator import generate_amp_envelope
+
+
+def plot_amp_envelope(envelope):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
