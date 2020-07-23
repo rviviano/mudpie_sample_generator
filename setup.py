8b1105220e9221fd22159bb76ae139d1706511ad
Raymond Viviano <rayviviano@gmail.com>
2020-07-23 01:06:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 def get_version():
-    # TODO: Test before using
+    # TODO: Test before using or find a better version tracker to import
     with open("mudpie_sample_generator.py", 'r') as f:
         for line in f:
             if "__version__" in line:
@@ -16,9 +16,10 @@ setup(name='mudpie-sample-generator',
       author='Raymond Viviano',
       author_email='rayviviano@gmail.com',
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      py_modules=['mudpie_sample_generator'],
       entry_points = { 
             'console_scripts': [ 
-                'mudsampgen = mudpie_sample_generator.py:main'
+                'mudsampgen = mudpie_sample_generator:main'
             ] 
         }, 
       license='LICENSE.md',
