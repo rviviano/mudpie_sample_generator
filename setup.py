ba6350d6a093ace88a45d9c2834e921fbd4cb48f
Raymond Viviano <rayviviano@gmail.com>
2020-07-23 00:07:30 -0400

    Update setup.py
    Add function get_version() to extract version from main script to ensure that these two files are consistent.

--- setup.py
+++ setup.py
@@ -2,8 +2,15 @@
 
 from setuptools import setup, find_packages
 
+def get_version():
+    with open("mudpie_sample_generator.py", 'r') as f:
+        for line in f:
+            if "__version__" in line:
+                return line.split("=")[1].strip()
+           
+
 setup(name='mudipie-sample-generator',
-      version='0.1.1',
+      version=get_version(),
       description='Extract random samples from wav file and save them.',
       author='Raymond Viviano',
       author_email='rayviviano@gmail.com',
