54440bd114332da761a06bc8b1c6ce7f567830cd
Raymond Viviano <rayviviano@gmail.com>
2020-07-22 22:17:22 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='mudipie-sample-generator',
+      version='0.1.0',
+      description='Extract random samples from wav file and save them.',
+      author='Raymond Viviano',
+      author_email='rayviviano@gmail.com',
+      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      license='LICENSE.md',
+    )
